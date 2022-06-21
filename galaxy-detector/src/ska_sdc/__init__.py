774a1992d7372629f187ad243693d40c469fa7b6
David Girón <56431707+duvidG@users.noreply.github.com>
2022-06-21 16:06:52 +0200

    Delete galaxy-detector/src/ska_sdc directory

--- galaxy-detector/src/ska_sdc/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.scoring.ska_sdc.sdc1.models.sdc1_score import Sdc1Score
-from src.scoring.ska_sdc.sdc1.sdc1_scorer import Sdc1Scorer
-from src.scoring.ska_sdc.sdc2.models.sdc2_score import Sdc2Score
-from src.scoring.ska_sdc.sdc2.sdc2_scorer import Sdc2Scorer
-
-__all__ = ["Sdc1Scorer", "Sdc2Scorer"]
